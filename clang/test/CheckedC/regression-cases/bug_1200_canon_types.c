79c5a66813c7f16a29f8a9defb3f2ea9fbcf91cb
David Tarditi <david.tarditi@checkedc.org>
2023-12-03 10:37:22 -0800

    Fix compiler assert involving a bound-safe interface with a typedef'ed type.
    The conversion of a pointer-typed variable with a bounds-safe interface to
    an unchecked pointer type was failing when the bound-safe interface type used
    a typedef'ed type. There was an assertion that a type had not been
    not been canonicalized. The fix is to use the canonicalized version of the
    bounds-safe interface type in the conversion code.

--- clang/test/CheckedC/regression-cases/bug_1200_canon_types.c
+++ clang/test/CheckedC/regression-cases/bug_1200_canon_types.c
@@ -1,12 +1,22 @@
-typedef char CHAR16;
+// This is a regression test case for
+//  https://github.com/checkedc/checkedc-llvm-project/issues/1200
+//
+// This test checks conversion of a pointer-typed variable with a
+// bounds-safe interface to an unchecked pointer type, wehere the
+// bound-safe interface type uses a typedef'ed type.
+//
+// RUN: %clang -cc1 -verify -fcheckedc-extension %s
+// expected-no-diagnostics
+
+typedef char CHAR;
 
 int
-FindAccessVariable (_Nt_array_ptr<CHAR16> VariableName) {
+FindAccessVariable (_Nt_array_ptr<char> VariableName) {
   return -1;
 }
 
 int
 mineVariableServiceGetVariable (
-CHAR16 *VariableName : itype(_Nt_array_ptr<CHAR16>)) {
+char  *VariableName : itype(_Nt_array_ptr<CHAR>)) {
 return FindAccessVariable (VariableName);
 }
