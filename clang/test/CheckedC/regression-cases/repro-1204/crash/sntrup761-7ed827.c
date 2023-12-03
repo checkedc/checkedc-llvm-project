b65cbb44b021f332a3df5d05e92ac11eaea8c6ff
David Tarditi <david.tarditi@checkedc.org>
2023-12-03 11:21:18 -0800

    Remove code.

--- clang/test/CheckedC/regression-cases/repro-1204/crash/sntrup761-7ed827.c
+++ clang/test/CheckedC/regression-cases/repro-1204/crash/sntrup761-7ed827.c
@@ -54398,64 +54398,11 @@ static void KEM_KeyGen(unsigned char *pk,unsigned char *sk)
 {
   int i;
 
-  ZKeyGen(pk,sk); sk += SecretKeys_bytes;
-  for (i = 0;i < PublicKeys_bytes;++i) *sk++ = pk[i];
-  randombytes(sk,Inputs_bytes); sk += Inputs_bytes;
-  Hash_prefix(sk,4,pk,PublicKeys_bytes);
+  ZKeyGen(pk,sk); // sk += SecretKeys_bytes;
+// for (i = 0;i < PublicKeys_bytes;++i) *sk++ = pk[i];
+ // randombytes(sk,Inputs_bytes); sk += Inputs_bytes;
+ // Hash_prefix(sk,4,pk,PublicKeys_bytes);
 }
-
-/* c,r_enc = Hide(r,pk,cache); cache is Hash4(pk) */
-static void Hide(unsigned char *c,unsigned char *r_enc,const Inputs r,const unsigned char *pk,const unsigned char *cache)
-{
-  Inputs_encode(r_enc,r);
-  ZEncrypt(c,r,pk); c += Ciphertexts_bytes;
-  HashConfirm(c,r_enc,pk,cache);
-}
-
-/* c,k = Encap(pk) */
-static void Encap(unsigned char *c,unsigned char *k,const unsigned char *pk)
-{
-  Inputs r;
-  unsigned char r_enc[Inputs_bytes];
-  unsigned char cache[Hash_bytes];
-
-  Hash_prefix(cache,4,pk,PublicKeys_bytes);
-  Inputs_random(r);
-  Hide(c,r_enc,r,pk,cache);
-  HashSession(k,1,r_enc,c);
-}
-
-/* 0 if matching ciphertext+confirm, else -1 */
-static int Ciphertexts_diff_mask(const unsigned char *c,const unsigned char *c2)
-{
-  uint16 differentbits = 0;
-  int len = Ciphertexts_bytes+Confirm_bytes;
-
-  while (len-- > 0) differentbits |= (*c++)^(*c2++);
-  return (1&((differentbits-1)>>8))-1;
-}
-
-/* k = Decap(c,sk) */
-static void Decap(unsigned char *k,const unsigned char *c,const unsigned char *sk)
-{
-  const unsigned char *pk = sk + SecretKeys_bytes;
-  const unsigned char *rho = pk + PublicKeys_bytes;
-  const unsigned char *cache = rho + Inputs_bytes;
-  Inputs r;
-  unsigned char r_enc[Inputs_bytes];
-  unsigned char cnew[Ciphertexts_bytes+Confirm_bytes];
-  int mask;
-  int i;
-
-  ZDecrypt(r,c,sk);
-  Hide(cnew,r_enc,r,pk,cache);
-  mask = Ciphertexts_diff_mask(c,cnew);
-  for (i = 0;i < Inputs_bytes;++i) r_enc[i] ^= mask&(r_enc[i]^rho[i]);
-  HashSession(k,1+mask,r_enc,c);
-}
-
-/* ----- crypto_kem API */
-
 
 int crypto_kem_sntrup761_keypair(unsigned char *pk,unsigned char *sk)
 {
@@ -54463,16 +54410,5 @@ int crypto_kem_sntrup761_keypair(unsigned char *pk,unsigned char *sk)
   return 0;
 }
 
-int crypto_kem_sntrup761_enc(unsigned char *c,unsigned char *k,const unsigned char *pk)
-{
-  Encap(c,k,pk);
-  return 0;
-}
-
-int crypto_kem_sntrup761_dec(unsigned char *k,const unsigned char *c,const unsigned char *sk)
-{
-  Decap(k,c,sk);
-  return 0;
-}
 #endif /* USE_SNTRUP761X25519 */
-# 1274 "sntrup761.c"
+
